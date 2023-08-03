9b062cb9f049ae9de590d84c152f50f708d025df
mldang <mldang@uw.edu>
2023-08-03 15:28:20 -0700

    ading a component

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from './Component';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Component/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>This is the second component: <Component/> that is a component^^^</p>
+
       </header>
     </div>
   );
---
+++ basic-app/src/Component.js
@@ -0,0 +1,9 @@
+function Component () {
+return (
+  <div>
+    <h1>This is a component</h1>
+  </div>
+)
+}
+
+export default Component
